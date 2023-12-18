ef1b9dbe401b918ba4e593ad5be9c1df066630d0
sachnaror <sachnaror@gmail.com>
2023-12-18 05:58:32 +0530

    Resolved merge conflicts in README.md

---
+++ README.py
@@ -0,0 +1,8 @@
+My quick 15 mins Django REST framework implementation 
+-----------------------------------------------------
+
+Ok, A short intro on what it is all about: Especially the DRF.
+
+Django REST framework (DRF) is a powerful and flexible toolkit for building Web APIs. Its main benefit is that it makes serialization much easier.
+
+Django REST framework is based on Django’s class-based views, so it’s an excellent option if you’re familiar with Django. It adopts implementations like class-based views, forms, model validator, QuerySet, and more.
